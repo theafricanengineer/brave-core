93578a2975e80ed53104b3f6c817b7a8a7c0240e
Simon Hong <shong@brave.com>
2020-04-28 19:26:56 +0900

    Use temporal AutocompleteClassifier to get proper navigation url
    To get proper selection navigation url for profile, use tempral
    AutocompleteClassfier. We can't use AutocompleteClassifierFactory
    for this because AutocompleteClassifierFactory doesn't create service
    for otr profile. But we need different navigation url for OTR profile.

--- chromium_src/chrome/browser/autocomplete/autocomplete_classifier_factory.cc
+++ chromium_src/chrome/browser/autocomplete/autocomplete_classifier_factory.cc
@@ -3,14 +3,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/autocomplete/brave_autocomplete_provider_client_for_classifier.h"
+#include "brave/browser/autocomplete/brave_autocomplete_provider_client.h"
 #include "brave/browser/autocomplete/brave_autocomplete_scheme_classifier.h"
 #include "brave/components/omnibox/browser/brave_autocomplete_controller.h"
 #include "components/omnibox/browser/autocomplete_classifier.h"
 #include "components/omnibox/browser/autocomplete_controller.h"
 
 #define AutocompleteController BraveAutocompleteController
-#define ChromeAutocompleteProviderClient BraveAutocompleteProviderClientForClassifier  // NOLINT
+#define ChromeAutocompleteProviderClient BraveAutocompleteProviderClient  // NOLINT
 #define ChromeAutocompleteSchemeClassifier BraveAutocompleteSchemeClassifier
 #include "../../../../../chrome/browser/autocomplete/autocomplete_classifier_factory.cc"
 #undef ChromeAutocompleteSchemeClassifier
