af5fb7c93184c4ba3ce97ef1a060c4984ca0ed3f
jamesmudgett <jmm.seasons@gmail.com>
2020-05-07 11:23:02 -0500

    add uuidv4 to auto-gen room name when none provided

--- components/brave_new_tab_ui/components/default/together/index.tsx
+++ components/brave_new_tab_ui/components/default/together/index.tsx
@@ -72,13 +72,20 @@ class Together extends React.PureComponent<Props, State> {
     )
   }
 
+  uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+      return v.toString(16)
+    })
+  }
+
   shouldCreateCall = (event: any) => {
     event.preventDefault()
 
     let { room } = this.state
 
     if (!room) {
-      room = 'coolroom'
+      room = this.uuidv4()
     }
 
     window.open(`https://together.brave.com/${room}`, '_self')
